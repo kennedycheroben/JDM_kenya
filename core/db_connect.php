bbe13c92986c9a6c6eb4512a065a567cbb149d83
cheroben <kennedycheroben001@gmail.com>
2026-09-06 22:43:29 +0300

    fix: include config.php in repository for cPanel deployment and disable persistent PDO connection

--- core/db_connect.php
+++ core/db_connect.php
@@ -76,7 +76,7 @@ try {
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false, 
-            PDO::ATTR_PERSISTENT => true,        
+            PDO::ATTR_PERSISTENT => false,        
         ]
     );
 } catch (PDOException $e) {
